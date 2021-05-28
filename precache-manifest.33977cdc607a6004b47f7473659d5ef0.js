652720072ee72bdf813baded3994c9c493d68861
KentIIX <jjayy14k@gmail.com>
2021-05-27 23:08:47 -0700

    Updates

---
+++ precache-manifest.33977cdc607a6004b47f7473659d5ef0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c29bb1f68f3462275adfdbc813772a5",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "c65f70c61cc118cb0cdb",
+    "url": "/Weather-App/static/css/main.8c665456.chunk.css"
+  },
+  {
+    "revision": "380d154867fa4f5f5072",
+    "url": "/Weather-App/static/js/2.d82b1bcc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Weather-App/static/js/2.d82b1bcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c65f70c61cc118cb0cdb",
+    "url": "/Weather-App/static/js/main.63543cfc.chunk.js"
+  },
+  {
+    "revision": "31e4190719e71ca1b747",
+    "url": "/Weather-App/static/js/runtime-main.71741c98.js"
+  }
+]);
